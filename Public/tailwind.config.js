08bb9b11027868a196f61595f1374da9dbfba1f4
fatawahid15 <sasaputri0705@gmail.com>
2024-09-01 20:11:50 +0700

    fix

--- Public/tailwind.config.js
+++ Public/tailwind.config.js
@@ -4,7 +4,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}"
+
   ],
   theme: {
     extend: {},
